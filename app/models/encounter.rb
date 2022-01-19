4ab4ab77c2e71021b7aedd17bba57e9ad145ff1d
Peter Venhuizen <pvenhuizen@sogyo.nl>
2022-01-19 12:20:01 +0100

    Add description and monster validations

--- app/models/encounter.rb
+++ app/models/encounter.rb
@@ -1,3 +1,5 @@
 class Encounter < ApplicationRecord
   validates :name, presence: true
+  validates :description, presence: true, length: { minimum: 10 }
+  validates :monsters, presence: { message: 'the encounter should have at least one' }
 end
