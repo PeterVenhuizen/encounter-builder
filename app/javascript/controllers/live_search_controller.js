c239d97b7ac8bfd9eac7faafb53d765e92d0b97c
Peter Venhuizen <pvenhuizen@sogyo.nl>
2022-01-30 16:56:15 +0100

    Add Stimulus live search config

---
+++ app/javascript/controllers/live_search_controller.js
@@ -0,0 +1,33 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = [ "results", "searchform" ]
+  timeout
+  pendingRequest
+
+  connect() {
+    // console.log("Connected!")
+  }
+
+  loading() {
+    this.pendingRequest = true
+    setTimeout(() => {
+      if(this.pendingRequest) {
+        this.resultsTarget.innerHTML = "<div>Searching...</div>"
+      }
+    }, 500)
+  }
+
+  search() {
+    clearTimeout(this.timeout)
+    this.timeout = setTimeout(() => {
+      Rails.fire(this.searchformTarget, 'submit')
+    }, 200)
+  }
+
+  handleResults() {
+    this.pendingRequest = false
+    const [data, status, xhr] = event.detail
+    this.resultsTarget.innerHTML = xhr.response
+  }
+}
