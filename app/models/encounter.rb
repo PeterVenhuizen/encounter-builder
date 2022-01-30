4e1b5d4a4a6cf098f4a1c42f00f46fcf3b31d7a1
Peter Venhuizen <pvenhuizen@sogyo.nl>
2022-01-30 16:02:48 +0100

    Destroy old models

--- app/models/encounter.rb
+++
@@ -1,5 +0,0 @@
-class Encounter < ApplicationRecord
-  validates :name, presence: true
-  validates :description, presence: true, length: { minimum: 10 }
-  validates :monsters, presence: { message: 'the encounter should have at least one' }
-end
