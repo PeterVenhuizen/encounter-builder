0b093bd9c5e3cba9c1303c4502c58b4d813bca3d
Peter Venhuizen <pvenhuizen@sogyo.nl>
2022-02-01 13:22:27 +0100

    Add encounter show and index styling

---
+++ app/presenters/encounter_presenter.rb
@@ -0,0 +1,16 @@
+class EncounterPresenter
+  attr_reader :difficulty
+
+  def initialize(encounter)
+    @encounter = encounter
+    @difficulty = encounter.summary[:difficulty]
+  end
+
+  def number_of_monsters
+    @encounter.fates.sum(&:group_size)
+  end
+
+  def created_at
+    @encounter.created_at.strftime('%d-%B-%Y')
+  end
+end
