4b0093af5ab9f5506cf24a4c674b6262516a9722
Peter Venhuizen <pvenhuizen@sogyo.nl>
2022-02-17 03:33:13 +0100

    Add monster stat block

---
+++ app/presenters/monster_presenter.rb
@@ -0,0 +1,40 @@
+class MonsterPresenter
+  def initialize(monster)
+    @monster = monster
+  end
+
+  def big_name
+    @monster.name.upcase
+  end
+
+  def size_type_and_alignment
+    "#{@monster.size} #{@monster.species}, #{@monster.alignment}".humanize
+  end
+
+  def hp
+    "#{@monster.hit_points} (#{@monster.hit_dice})"
+  end
+
+  def shorten(ability)
+    ability.slice(0, 3).upcase
+  end
+
+  def score_and_modifier(score)
+    mod = prepend_sign((score.to_i - 10) / 2)
+    "#{score} (#{mod})"
+  end
+
+  def challenge_and_xp
+    "#{@monster.challenge_rating} (#{ActiveSupport::NumberHelper.number_to_delimited(@monster.xp)} XP)"
+  end
+
+  def proficiency_bonus
+    prepend_sign(@monster.proficiency_bonus)
+  end
+
+  private
+
+  def prepend_sign(value)
+    "%+d" % value
+  end
+end
