1d918b41d544f1bfddc8b0d462a605bdc9a03df5
Peter Venhuizen <pvenhuizen@sogyo.nl>
2022-02-22 15:36:49 +0100

    Transform players and monsters to combatants

--- app/models/combat_tracker.rb
+++ app/models/combat_tracker.rb
@@ -7,11 +7,20 @@ class CombatTracker
     @encounter = encounter
     @round = 1
     @turn = 1
+    transform_to_combatants
   end
 
   private
 
-  def init_combatants
-    
+  def transform_to_combatants
+    # add players
+    @combatants = @encounter.players.map { |p| Combatant.new(parent: p) }
+
+    # monsters
+    @encounter.fates.each do |fate|
+      fate.group_size.times do
+        @combatants << Combatant.new(parent: Monster.find_by(id: fate.monster_id))
+      end
+    end
   end
 end
