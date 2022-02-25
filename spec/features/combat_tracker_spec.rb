d9b0d31977fd5e2317a7a9514c106ca740fbffb1
Peter Venhuizen <pvenhuizen@sogyo.nl>
2022-02-25 14:08:00 +0100

    Feature test stub

--- spec/features/combat_tracker_spec.rb
+++ spec/features/combat_tracker_spec.rb
@@ -1,7 +1,46 @@
 require 'rails_helper'
 
 RSpec.descibe 'Combat Tracker', type: :feature, js: true do
-  scenario 'is started and saved' do
-    #visit combat_tracker_path
+  fixtures :monsters, :parties, :players
+
+  let(:encounter_attributes) do
+    {
+      name: 'Two bandits',
+      description: 'Two bandits and a cat',
+      fates_attributes: [
+        { monster_id: monsters(:bandit).id, group_size: 2 },
+        { monster_id: monsters(:cat).id }
+      ]
+    }
   end
+
+  before(:each) do
+    party = parties(:party_of_three)
+    @encounter = party.encounters.create(encounter_attributes)
+    @combat_tracker = @encounter.create_combat_tracker
+  end
+
+  scenario 'is started' do
+    # go to encounter
+    visit encounters_path @encounter
+
+    # click on play encounter
+    expect {
+      click_on 'Play Encounter'
+    }.to change(Combatant, :count).by(1)
+
+    # assert redirect_to combat_tracker show
+    expect(response).to render_template 'combat_trackers/show'
+    expect(page).to have_css '.alert-success'
+    expect(page).to have_text 'Combat Tracker'
+  end
+
+  scenario 'initiative is set and turns are played'
+    # go to combat_tracker show
+    # post initiative
+    # assert combatants ordered by initiative
+    # assert turn goes back to one after five
+    # assert round + 1
+  scenario 'is continued'
+  scenario 'is destroyed'
 end
